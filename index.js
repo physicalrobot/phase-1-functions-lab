05c73dc857f3d5a9dd50f6b80e626e4085bc2677
Vikalp Malhotra <kimboho@gmail.com>
2021-12-13 16:04:46 -0500

    commit tree dea8978382dbfb6afdfde29bc60b14a3c77a15a6

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    let blocks;
+    return blocks = Math.abs(someValue - 42);
+}
+
+function distanceFromHqInFeet(someValue) {
+    let blocks = distanceFromHqInBlocks(someValue);
+    let hqFeet;
+
+    return hqFeet = blocks * 264;
+
+}
+
+function distanceTravelledInFeet(start, destination) {
+    let feet;
+    return feet = Math.abs(start - destination) * 264;
+
+
+}
+function calculatesFarePrice(start, destination) {
+    let distance = distanceTravelledInFeet(start, destination);
+    if (distance <= 400) {
+        return 0;
+
+    } else if (distance <= 2000) {
+
+        let money = (distance - 400) * .02
+
+        return money;
+
+    } else if (distance <= 2500 && distance > 2000) {
+        let difference = 2500 - distance;
+
+        let newmoney = 25;
+        return newmoney;
+
+    } else {
+        return 'cannot travel that far';
+
+    }
+
+
+
+
+
+
+}
